17235c6700eee76aaf229042d02ab7f71c8cb5d7
andrerodrigues <andrerodrigues29101992@gmail.com>
2022-07-22 22:44:56 +0100

    added router

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,18 @@
+import Homepage from "pages/Homepage/Homepage";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "styles/global.scss";
 
 const App = () => {
-  return <div>My CRA template</div>;
+  return (
+    <>
+      {/*   <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+      {/*   <Footer /> */}
+    </>
+  );
 };
 
 export default App;

fcbeb24c1398109fc19f39c75fde3f804fee13db
andrerodrigues <andrerodrigues29101992@gmail.com>
2022-08-31 14:30:31 +0100

    Add close to navbar and corrected navbar

--- src/components/AppNavbar/AppNavbar.jsx
+++ src/components/AppNavbar/AppNavbar.jsx
@@ -34,12 +34,23 @@ const AppNavbar = () => {
       >
         <Offcanvas.Header closeButton />
         <Offcanvas.Body>
-          <Nav className={styles.offcanvasLinks}>
-            <Nav.Link href="/works">Trabalhos</Nav.Link>
+          <Nav
+            onSelect={() => setShow(false)}
+            className={styles.offcanvasLinks}
+          >
+            <Nav.Link as={Link} to="/works" href="/works">
+              Trabalhos
+            </Nav.Link>
             <br />
             {/*  <Nav.Link href="/blog">Blog</Nav.Link> <br /> */}
-            <Nav.Link href="/about">Sobre</Nav.Link> <br />
-            <Nav.Link href="/contact">Contactos</Nav.Link> <br />
+            <Nav.Link as={Link} to="/about" href="/about">
+              Sobre
+            </Nav.Link>{" "}
+            <br />
+            <Nav.Link as={Link} to="/contact" href="/contact">
+              Contactos
+            </Nav.Link>{" "}
+            <br />
           </Nav>
         </Offcanvas.Body>
       </Navbar.Offcanvas>
